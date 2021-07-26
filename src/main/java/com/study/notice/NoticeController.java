8d7acb84a5a4589e633220b3e46c75dbad05eafe
SecretFr <linkclean@naver.com>
2021-07-26 13:27:59 +0900

    공지

--- src/main/java/com/study/notice/NoticeController.java
+++ src/main/java/com/study/notice/NoticeController.java
@@ -28,16 +28,16 @@ public class NoticeController {
 	@Qualifier("com.study.model.NoticeServiceImpl")
 	private NoticeService service;
 
-	@GetMapping("/")
-	public String home(Locale locale, Model model) {
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-		return "/home";
-	}
+//	@GetMapping("/")
+//	public String home(Locale locale, Model model) {
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//
+//		String formattedDate = dateFormat.format(date);
+//
+//		model.addAttribute("serverTime", formattedDate);
+//		return "/home";
+//	}
 
 	@GetMapping("create")
 	public String create() {
